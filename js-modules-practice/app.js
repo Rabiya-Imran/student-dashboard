32cbadfc523c1760259f183bafb54116005bc637
Rabiya-Imran <217018975+Rabiya-Imran@users.noreply.github.com>
2026-06-22 21:31:18 +0500

    Delete js-modules-practice directory

--- js-modules-practice/app.js
+++
@@ -1,6 +0,0 @@
-import pi, {add, subtract} from './mathUtils.js'; // Importing default and named exports
-import {capitalize} from './stringUtils.js'; 
-console.log("Value of pi:", pi);
-console.log("Addition of 5 and 3:", add(5,3));
-console.log("Subtraction of 5 and 3:", subtract(5,3));
-console.log("Capitalized string:", capitalize("rabiya"));
--- js-modules-practice/utils/stringUtils.js
+++
@@ -1,3 +0,0 @@
-export function capitalize(str){
-return str.toUpperCase();
-}
